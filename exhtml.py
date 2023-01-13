742c55ed0202172fd403d978a5532b4b00618ba7
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-01-13 15:56:25 +0900

    add DMC-12

--- exhtml.py
+++ exhtml.py
@@ -3,6 +3,11 @@ import sys
 import os
 from itertools import count
 
+def listrfind(list,target):
+    for i in range(len(list)-1,-1,-1):
+        if target in list[i]:
+            return i
+
 burl=""
 if len(sys.argv) <= 1:
     print("URLがありません")
@@ -11,7 +16,12 @@ else:
     burl = sys.argv[1]
 
 body = []
-exp = 0
+exp = False
+gen = False
+tan = ""
+exbegin = 0
+brand = ["トミカ","ホットウィール","マジョレット","チョロＱ","ディーラー","京商","コーヒーのおまけ","マッチボックス","ウェリー","RMZ","マイスト","グリーンライト"]
+brandsum=len(brand)
 try:
     os.mkdir("exchanged")
 except FileExistsError:
@@ -19,46 +29,76 @@ except FileExistsError:
 finally:
         try:
             all = urllib.request.urlopen(burl).readlines()
-            for h in range(len(all)):
-                all[h] = all[h].decode("utf-8")
-            for i in count():
-                if "article_title" in all[i]:
-                    title = all[i+1][all[i+1].find(">")+1:all[i+1].rfind("<")]
-                    if "/" in title:
-                        title = title.replace("/","_")
-                elif "lab_article_body" in all[i]:
-                    body_begin = i
-                    for j in count(i):
-                        if "</div>" in all[j]:
-                            body_end = j
-                            break
-                    break
-            for k in range(body_begin+1,body_end):
-                bodybase = all[k]
-                bodyline = all[k][all[k].find(">")+1:all[k].rfind("<")]
-                if exp == 1:
-                    exp = 0
-                    bodyline = '<p class="car_explain">\n            '+ bodyline
-                elif "<blockquote>" in bodybase:
-                    bodyline = '<h3 class="header_txt" id="car_name_">'+bodyline+"</h3>"
-                    exp = 1
-                elif "</a>" in bodyline:
-                    bodyline = bodyline[bodyline.find(">")+1:bodyline.rfind("<")]
-                    url = all[k][all[k].find("href=")+6:all[k].find("rel")-2]
-                    if bodyline != url:
-                        bodyline = '<a class="car_link" href="'+url+'">'+bodyline+'</a><br>'
-                    else:
-                        bodyline = '<a class="car_link" href="'+url+'"></a><br>'
-                elif 
-                body.append(bodyline)
-            with open(title+".html",'w+',encoding='utf-8') as ex:
-                ex.write('<!DOCTYPE html>\n<html lang="ja" id="main_html">\n    <head>\n        <link rel="shortcut icon" href="https://raw.githubusercontent.com/Tastuaki/car_index/main/favicon.ico" type="image/vnd.microsoft.icon">\n        <link rel="stylesheet" href="https://tastuaki.github.io/car_index/car_index.css">\n    </head>\n    <body>\n        <header>\n            <h1 class="header_txt" id="main_title"></h1>\n        </header>\n        <div id="main">\n')
-                for i in range(len(body)):
-                    writeline = body[i].replace("	","")
-                    if writeline != "":
-                        ex.write("            "+writeline+"\n")
-                ex.write('        </div>\n        <footer>\n            <a class="footer_link" href="https://tastuaki.github.io/car_index/index.html">ホーム</a>　\n            <a class="footer_link" href="https://tastuaki.github.io/car_index/brand/new.html">新製品カレンダー</a>\n            </footer>\n  </body>\n</html>\n')
-            print("exchange "+ burl)
         except urllib.error.URLError:
             print("\""+burl+"\""+"Not Found")
             exit()
+        for h in range(len(all)):
+            all[h] = all[h].decode("utf-8")
+        for i in count():
+            if "article_title" in all[i]:
+                title = all[i+1][all[i+1].find(">")+1:all[i+1].rfind("<")]
+                titletext='            <h1 class="header_txt" id="main_title">'+title+'</h1>\n        </header>\n        <div id="main">\n'
+                if "/" in title:
+                    title = title.replace("/","_")
+            elif "lab_article_body" in all[i]:
+                body_begin = i
+                for j in count(i):
+                    if "</div>" in all[j]:
+                        body_end = j
+                        break
+                break
+        for k in range(body_begin+1,body_end):
+            bodybase = all[k]
+            bodyline = all[k][all[k].find(">")+1:all[k].rfind("<")]
+            if gen:
+                gen = False
+                body.append(tan)
+                tan = ""
+            if "</a>" in bodyline:
+                bodyline = bodyline[bodyline.find(">")+1:bodyline.rfind("<")]
+                url = all[k][all[k].find("href=")+6:all[k].find("rel")-2]
+                if bodyline != url:
+                    bodyline = '<a class="car_link" href="'+url+'">'+bodyline+'</a><br>'
+                else:
+                    bodyline = '<a class="car_link" href="'+url+'"></a><br>'
+            elif exp:
+                exp = False
+                if "。" in bodyline:
+                    tan = bodyline
+                    bodyline = '<p class="car_explain">'
+                    gen = True
+                else:
+                    bodyline = '<p class="car_explain">\n            '+bodyline
+            elif "<blockquote>" in bodybase:
+                if "代" in bodyline:
+                    bodyline = '<b>'+bodyline+'</b>'
+                else:
+                    bodyline = '<h3 class="header_txt" id="car_name_">'+bodyline+"</h3>"
+                    exp = 1
+            elif "<br>" in bodybase:
+                bodyline = "</p>"
+            else:
+                for i in range(brandsum):
+                    if brand[i] in bodyline:
+                        bodyline = '<p class="brand">'+bodyline+'</p>'
+                        break
+            body.append(bodyline)
+            if "<b>" in bodyline:
+                endbody = len(body)-1
+                newbody = body[endbody]
+                if "car_explain" not in newbody:
+                    body[endbody] = '<p class="car_explain">'
+                    body.append(newbody)
+            elif "</p>" == bodyline:
+                endbody = len(body)
+                exbegin = listrfind(body,"car_explain")
+                for j in range(exbegin+1,endbody-1):
+                    body[j] = "    "+body[j]+"<br>"
+        with open(title+".html",'w+',encoding='utf-8') as ex:
+            ex.write('<!DOCTYPE html>\n<html lang="ja" id="main_html">\n    <head>\n        <link rel="shortcut icon" href="https://raw.githubusercontent.com/Tastuaki/car_index/main/favicon.ico" type="image/vnd.microsoft.icon">\n        <link rel="stylesheet" href="https://tastuaki.github.io/car_index/car_index.css">\n    </head>\n    <body>\n        <header>\n'+titletext)
+            for i in range(len(body)):
+                writeline = body[i].replace("	","")
+                if writeline != "":
+                    ex.write("            "+writeline+"\n")
+            ex.write('        </div>\n        <footer>\n            <a class="footer_link" href="https://tastuaki.github.io/car_index/index.html">ホーム</a>　\n            <a class="footer_link" href="https://tastuaki.github.io/car_index/brand/new.html">新製品カレンダー</a>\n            </footer>\n  </body>\n</html>\n')
+        print("exchange "+ burl+":"+title)
