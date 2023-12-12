fd4d316a5728f33578548b19c6af25e0659c835b
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-12-12 14:35:01 +0900

    nmkf

---
+++ nmkf.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+base = r"C:\Users\Tastuaki\repo" + "\\"
+
+# with open(base+"ti","w+",encoding="utf-8") as f:
+#     data = f.readlines()
+
+def mfile(fname,end=False):
+    e = ""
+    try:
+        fname = base +fname
+        # print(fname)
+        os.mkdir(fname)
+        if end:
+            print("\nmake "+fname)
+        return True
+    except FileExistsError:
+        if end:
+            print("\nError! \""+fname+"\" is already exist")
+        return False
+
+# for d in data:
+bname = ""
+while True:
+    print("input file name:",end="")
+    bname = input()
+
+    if bname == "-1":
+        break
+
+    fname = ""
+    for f in bname.split("|"):
+        fname += f + "\\"
+        e = mfile(fname)
+    
+    if e:
+        print("make " + fname)
+    else:
+        print("already exist "+fname)
+
+print("\nFinish! Press any Key...",end="")
+input()
